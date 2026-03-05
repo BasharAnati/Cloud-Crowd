0885c41b4b7b32bf82f8d2e63488e359c3270ba9
BasharAnati <101931358+BasharAnati@users.noreply.github.com>
2026-03-05 05:59:17 +0300

    Update password for user Mai

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
   }
 
   const USERS = [
-    { username: "Mai", password: "000000**" },
+    { username: "Mai", password: "M#123" },
     { username: "Tuleen", password: "000000**" },
     { username: "Anati", password: "A@1995" },
     { username: "Aser", password: "000000**" },
